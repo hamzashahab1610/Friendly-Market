b615f4341d6e1d05938d1c569a60f737c0ecaf45
hamzashahab1610 <16102000.hamzashahab@gmail.com>
2019-11-23 12:45:35 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-za7ref
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-za7ref)
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+var firebaseConfig = {
+    apiKey: "AIzaSyDGj0Nt8zgnW1gePeKpppihTVm_tXpFLJ4",
+    authDomain: "friendlymarket1610.firebaseapp.com",
+    databaseURL: "https://friendlymarket1610.firebaseio.com",
+    projectId: "friendlymarket1610",
+    storageBucket: "friendlymarket1610.appspot.com",
+    messagingSenderId: "454594470697",
+    appId: "1:454594470697:web:363380e793468d1b3dcd38"
+  };
+
+export default firebaseConfig;
